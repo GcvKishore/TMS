59bf42853a3ef451e09af3a5522e8a8b83ad3c1c
Vedavyas Vundyala <vedvund@gmail.com>
2022-03-03 17:00:05 +0530

    Editing files - framework/javaScript/exam.js - framework/exam.html
    divided into small tasks(functions) and assigned to team members

---
+++ framework/javaScript/exam.js
@@ -0,0 +1,147 @@
+// Load json -- Load json and store it in a global variable ==> exam_details (Chaitanya)
+
+
+
+// Global Variables
+let current_section = 0;
+let current_question = 0;
+let start_time_stamp;
+let end_time_stamp;
+let current_question_details;
+let current_question_type;
+let user_inputs = [];
+
+
+// Start exam (Chaitanya)
+function startExam(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+//Display Questions (Amulya)
+function displayQuestion(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// Start countdown (Priyusha)
+function startCountDown(max_time){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// Generate questions based on questionType
+// Multiple Choice - Multiple Answers (Priyusha)
+function generateMCMA(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// Fill in the blanks (Vedavyas)
+function generateFITB(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// Short Answer (Amulya)
+function generateSA(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// File Submission (Chaitanya)
+function generateFU(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// on next button click
+// record user_inputs (Vedavyas)
+function onNextClick(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+
+// record user_inputs (Chaitanya)
+function recordUserInputs(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+
+// get user answers based on question type
+// Multiple Choice - Multiple Answers (Priyusha)
+function recordMCMA(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// Fill in the blanks (Vedavyas)
+function recordFITB(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// Short Answer (Amulya)
+function recordSA(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// File Submission (Chaitanya)
+function recordFS(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// evaluate user Answers (Priyusha)
+function evaluateUserAnswer(user_answers, actual_answers){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+// Show exam overview and highlight current question (optional)
+function examOverview(){
+    // Summary:
+    // para:
+    // return:
+
+}
+
+
+// generate exam summary json file (Optional)
+function createExamSummary(){
+    // Summary:
+    // para:
+    // return:
+
+}
