0879eafe8c7089c5e367d4ec4cb58e52a433977a
Gcv Kishore <gcvkishore@msitprogram.net>
2022-04-03 20:17:08 +0530

    Code updated for timer tutest.

--- website/views.py
+++ website/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'website/index.html')
+def underConstruction(request):
+    return render(request, 'website/underConstruction.html')
