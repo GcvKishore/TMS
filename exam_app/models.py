d7e399814d0b2d4b2e109797b025de48aab03ef1
vedvund <vedvund@gmail.com>
2022-04-11 16:09:44 +0530

    Add table - models.py
    Added new tables to database to store user inputs

--- exam_app/models.py
+++ exam_app/models.py
@@ -44,3 +44,32 @@ class Answer(models.Model):
 
     def __str__(self):
         return f"{self.answer}-{self.index}"
+
+
+class UserExamDetails(models.Model):
+    username = models.CharField(max_length=64)
+    exam = models.ForeignKey('MakeExam', on_delete=models.CASCADE)
+    status = models.CharField(max_length=64, blank=True)
+    result_status = models.CharField(max_length=64, blank=True)
+    overall_points = models.IntegerField(blank=True)
+    time_elapsed = models.DurationField(null=True)
+
+
+class UserQuestionDetails(models.Model):
+    question = models.ForeignKey('MakeQuestion', on_delete=models.CASCADE)
+    exam = models.ForeignKey('UserExamDetails', on_delete=models.CASCADE)
+    points = models.IntegerField(blank=True)
+    evaluation_status = models.CharField(max_length=64, blank=True)
+    time_elapsed = models.DurationField(null=True)
+
+
+class UserAnswerTextInput(models.Model):
+    question = models.ForeignKey('UserQuestionDetails', on_delete=models.CASCADE)
+    answer_text_input = models.TextField(blank=True)
+    index = models.IntegerField(blank=True)
+
+
+class UserAnswerFileUpload(models.Model):
+    question = models.ForeignKey('UserQuestionDetails', on_delete=models.CASCADE)
+    answer_text_input = models.TextField(blank=True)
+    index = models.IntegerField(blank=True)
