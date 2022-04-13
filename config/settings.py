fb48b3c2b9b08a7c59d741431d250eea50b48bb5
vedvund <vedvund@gmail.com>
2022-04-13 22:36:11 +0530

    Fix view-all-exams-instructor.html bug

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='abc')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vedvund-tutest.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fsd-tutest.herokuapp.com']
 
 # Application definition
 
